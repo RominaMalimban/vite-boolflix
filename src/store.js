c90eeb001205ac252d95e2d72b340fd8b5135f1e
RominaMalimban <romina.lyn@hotmail.it>
2022-12-16 13:49:48 +0100

    importazione store.js in App

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    apiUrl: "https://api.themoviedb.org/3/search/movie/?api_key=9dedd5f24457aa0b2af960a8c2a32689"
+    movieList: [],
+    apiUrl: "https://api.themoviedb.org/3/movie/popular",
+    apiKey: "9dedd5f24457aa0b2af960a8c2a32689"
 });
